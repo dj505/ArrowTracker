25cdb2ccdaea015fcba602b1e51adea24379e4b4
dj505 <dj505@users.noreply.github.com>
2019-04-29 13:09:43 -0600

    Mobile doesn't look like total crap

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,9 +5,9 @@ from app import songlist_pairs
 class SearchForm(FlaskForm):
     filters = (
     ("ranked-score", "Ranked, Score"),
-    ("ranked-difficulty", "Ranked, Difficulty")
+    ("ranked-difficulty", "Ranked, Difficulty"),
     ("unranked-score", "Ranked, Score"),
-    ("unranked-difficulty", "Ranked, Difficulty")
+    ("unranked-difficulty", "Ranked, Difficulty"))
     song = SelectField('Song', coerce=str, choices=songlist_pairs)
     filters = SelectField('Filter', coerce=str, choices=filters)
     userfilter = StringField('User [OPTIONAL]')
