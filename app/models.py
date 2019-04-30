e214e39a875eba00a2642e8512cbca79c35e84de
dj505 <dj505@users.noreply.github.com>
2019-04-29 18:51:07 -0600

    Added Unlocks list to resources

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file =db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    rivals = db.Column(db.ARRAY(db.Integer()), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
