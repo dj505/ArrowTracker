5ac828ad6593fb8f5b43ff990322df12f1a3fc16
dj505 <dj505@users.noreply.github.com>
2019-05-10 17:54:05 -0600

    Archiving works now

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from app import create_app
+from flask_apscheduler import APScheduler
+from weekly import randomize_weekly
 
+class Config(object):
+    SCHEDULER_API_ENABLED = True
+
+scheduler = APScheduler()
 app = create_app()
 
+@scheduler.task('cron', id='job', week='*', day_of_week='fri', hour='17', minute='53')
+def job():
+    randomize_weekly(app)
+
 if __name__ == '__main__':
+    scheduler.init_app(app)
+    scheduler.start()
     app.run(debug=True)
--- weekly.py
+++ weekly.py
@@ -1,35 +1,49 @@
 from loadsongs import load_song_lists
-import random
-import os
-import json
+from app.models import WeeklyPost
+from app import db, current_app
 import datetime
-import schedule
-import time
-from threading import Thread
+import random
+import json
+import app
+import os
 
 def get_current_weekly():
     with open('weekly.json', 'r') as f:
         weeklylist = json.load(f)
     return weeklylist['song']
 
-def randomize_weekly():
+def create_json(item):
+    scoredict = {
+        item.id: {
+            'song': item.song,
+            'difficulty': item.difficulty,
+            'lettergrade': item.lettergrade,
+            'score': item.score,
+            'stagepass': item.stagepass,
+            'platform': item.platform,
+            'ranked': item.ranked,
+            'author': item.author.username
+        }
+    }
+    rootdir = os.path.join(current_app.root_path, f'static/archived_weekly')
+    datedir = os.path.join(current_app.root_path, f'static/archived_weekly', datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    if not os.path.exists(datedir):
+        os.makedirs(datedir)
+    jsonfile = os.path.join(rootdir, datedir, f'{item.id}.json')
+    with open(jsonfile, 'w+') as f:
+        json.dump(scoredict, f, indent=2)
+
+def randomize_weekly(app):
     songs = load_song_lists()
     with open('weekly.json', 'r') as f:
         weeklyjson = json.load(f)
-    weeklyjson['song'] =  random.choice(load_song_lists())[0]
+    weeklyjson['song'] = random.choice(load_song_lists())[0]
     print(weeklyjson)
     with open('weekly.json', 'w') as f:
         json.dump(weeklyjson, f, indent=2)
-
-schedule.every().friday.at("13:51").do(randomize_weekly)
-
-class isitfridayyet(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.daemon=True
-        self.start()
-    def run(self):
-        while True:
-            schedule.run_pending()
-
-#isitfridayyet()
+    with app.app_context():
+        posts = db.session.query(WeeklyPost).all()
+        for item in posts:
+            create_json(item)
+        db.session.query(WeeklyPost).delete()
+        db.session.commit()
