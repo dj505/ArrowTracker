6b3f0e212ff9bfa8f494068cc73bb0ebc92f1faf
dj505 <dj505@users.noreply.github.com>
2019-05-17 17:27:51 -0600

    reset emails are spam apparently

--- app/users/routes.py
+++ app/users/routes.py
@@ -85,7 +85,7 @@ def reset_request():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         send_reset_email(user)
-        flash('A reset email has been reset with instructions!', 'info')
+        flash('A reset email has been reset with instructions! Make sure you check your spam folder!', 'info')
         return redirect(url_for('users.login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
 
