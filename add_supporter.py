1e0cb42023e18472cbd1f70940ad8f04025786dd
dj505 <dj505@users.noreply.github.com>
2019-07-24 19:27:02 -0600

    Improved add_supporter script

--- add_supporter.py
+++ add_supporter.py
@@ -1,7 +1,12 @@
 import json
+import os
 
-with open('supporters.json', 'r') as f:
-    data = json.load(f)
+try:
+    with open('supporters.json', 'r') as f:
+        data = json.load(f)
+except:
+    print('No supporters file exists, will create')
+    data = {}
 
 name = input('Name: ')
 amount = input('Amount: ')
@@ -12,5 +17,5 @@ if not amount.startswith('$'):
 data[name] = {}
 data[name]['amount'] = amount
 
-with open('supporters.json', 'w') as f:
+with open('supporters.json', 'w+') as f:
     json.dump(data, f, indent=2)
